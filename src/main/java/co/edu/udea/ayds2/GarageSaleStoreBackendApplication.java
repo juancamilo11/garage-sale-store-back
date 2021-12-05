9a93ec494a39a65d416db50ca2bc2bd039ed52ac
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-12-05 00:04:00 -0500

    Setup inicial del proyecto

---
+++ src/main/java/co/edu/udea/ayds2/GarageSaleStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package co.edu.udea.ayds2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageSaleStoreBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GarageSaleStoreBackendApplication.class, args);
+    }
+
+}
